cfb494d77701f3a5b51bf4a2c4836132de531753
Drew Owen <anowen@microsoft.com>
2016-12-12 23:01:19 -0800

    build full output obj, remove emDash

--- main.js
+++ main.js
@@ -1,11 +1,66 @@
 let fs = require('fs');
 let request = require('request');
-let cheerio = require('cheerio')
+let cheerio = require('cheerio');
 
-//TODO: remove "-" from output
-//TODO: write teams to output obj as well
+//TODO: pull team names from row header
 
-let baseUrl = 'http://en.wikipedia.org/w/api.php?action=parse&page=2016%E2%80%9317_Premier_League&prop=text&section=9&format=json';
+var _TeamNameToIndexMapping = {
+	"ARSENAL" : 0,  
+	"BOURNEMOUTH": 1, 
+	"BURNLEY": 2,
+	"CHELSEA": 3, 
+	"CRYSTAL PALACE": 4,
+	"EVERTON": 5,
+	"HULL": 6,
+	"LEICESTER CITY": 7,
+	"LIVERPOOL": 8,
+	"MANCHESTER CITY": 9,
+	"MANCHESTER UNITED": 10,
+	"MIDDLESBROUGH": 11,
+	"SOUTHAMPTON": 12,
+	"STOKE CITY": 13,
+	"SUNDERLAND": 14,
+	"SWANSEA CITY": 15,
+	"TOTTENHAM HOTSPUR": 16,
+	"WATFORD": 17,
+	"WEST BROMWICH ALBION": 18,
+	"WEST HAM UNITED": 19
+};
+
+var _TeamNamesByIndex = [
+	"ARSENAL", 
+	"BOURNEMOUTH",
+	"BURNLEY", 
+	"CHELSEA", 
+	"CRYSTAL PALACE",
+	"EVERTON",
+	"HULL",
+	"LEICESTER CITY",
+	"LIVERPOOL",
+	"MANCHESTER CITY",
+	"MANCHESTER UNITED",
+	"MIDDLESBROUGH",
+	"SOUTHAMPTON",
+	"STOKE CITY",
+	"SUNDERLAND",
+	"SWANSEA CITY",
+	"TOTTENHAM HOTSPUR",
+	"WATFORD",
+	"WEST BROMWICH ALBION",
+	"WEST HAM UNITED"
+	];
+
+let baseUrl = 'http://en.wikipedia.org/w/api.php?action=parse&page=2016%E2%80%9317_Premier_League&prop=text&section=10&format=json';
+
+function transformResult(result) {
+	if (!!result && result == "—") {
+		return null;
+	}
+	else if (!!result) {
+		return result.split('–').map(Number);
+	}
+	return result;
+}
 
 request(baseUrl, function (error, response, body) {
 	if (!error && response.statusCode == 200) {
@@ -34,12 +89,14 @@ request(baseUrl, function (error, response, body) {
 							result = td.lastChild.data;
 						}
 					}
-					currentRowArray[j] = result;
+					currentRowArray[j] = transformResult(result);
 				});
 				resultsArray[i - 1] = currentRowArray;
 			}
 		});
-		let output = JSON.stringify(resultsArray);
+		
+		var outputObject = {_TeamNamesByIndex, _TeamNameToIndexMapping, resultsArray };
+		let output = JSON.stringify(outputObject);
 		fs.writeFile("output.JSON", output, function (err) {
 			if (err) {
 				console.log("ERROR: " + err.message)
